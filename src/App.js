5403b3529ffd1464f14222d626351121bd26ac22
Joaquim Barreto <joaquim@barreto.co>
2019-03-11 00:40:33 +0000

    name and image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import HomepageLayout from "./HomepageLayout";
+import HomepageLayout from "./components/HomepageLayout";
 
 class App extends Component {
   render() {
