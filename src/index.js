3d29aefc286bd57a85472185d60d74ba8fe7f862
Joaquim Barreto <joaquim@barreto.co>
2019-03-28 11:11:18 +0000

    hashrouter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
