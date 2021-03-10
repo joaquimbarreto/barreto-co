1259e95f84e902725bcc1068e003289837cb0f64
Joaquim Barreto <joaquim@barreto.co>
2021-03-10 23:29:50 +0100

    components setup and languages in translation.json

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -2,40 +2,45 @@ import React from 'react';
 import { Button, Container, Divider, Header, Segment } from 'semantic-ui-react';
 import ResponsiveContainer from '../containers/ResponsiveContainer';
 import Footer from '../containers/Footer';
+import { useTranslation } from 'react-i18next';
 
-const Projects = () => (
-  <ResponsiveContainer>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Divider
-          as="h4"
-          className="header"
-          horizontal
-          style={{ margin: '2em 0em', textTransform: 'uppercase' }}
-        >
-          <Header as="h1" style={{ fontSize: '3em' }}>
-            Blog
+const Projects = () => {
+  const { t } = useTranslation();
+
+  return (
+    <ResponsiveContainer>
+      <Segment style={{ padding: '8em 0em' }} vertical>
+        <Container text>
+          <Divider
+            as="h4"
+            className="header"
+            horizontal
+            style={{ margin: '2em 0em', textTransform: 'uppercase' }}
+          >
+            <Header as="h1" style={{ fontSize: '3em' }}>
+              Blog
+            </Header>
+          </Divider>
+          <Header as="h3" style={{ fontSize: '2em' }}>
+            {t('adventures')}
           </Header>
-        </Divider>
-        <Header as="h3" style={{ fontSize: '2em' }}>
-          Adventures in Software Engineering
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Going from economist to photographer/filmmaker to software engineer
-          does have its perils. Explore Joaquim's trials and tribulations as he
-          graples with the information age world.
-        </p>
-        <Button
-          as="a"
-          size="huge"
-          href="https://www.fullstacksoftware.engineer"
-          target="_blank"
-        >
-          fullstacksoftware.engineer
-        </Button>
-      </Container>
-    </Segment>
-    <Footer />
-  </ResponsiveContainer>
-);
+          <p style={{ fontSize: '1.33em' }}>
+            Going from economist to photographer/filmmaker to software engineer
+            does have its perils. Explore Joaquim's trials and tribulations as
+            he graples with the information age world.
+          </p>
+          <Button
+            as="a"
+            size="huge"
+            href="https://www.fullstacksoftware.engineer"
+            target="_blank"
+          >
+            fullstacksoftware.engineer
+          </Button>
+        </Container>
+      </Segment>
+      <Footer />
+    </ResponsiveContainer>
+  );
+};
 export default Projects;
--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,54 +1,59 @@
 import React from 'react';
 import { Container, Grid, Header, List, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-const Footer = () => (
-  <Segment inverted vertical style={{ padding: '5em 0em' }}>
-    <Container>
-      <Grid divided inverted stackable>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="About" />
-            <List link inverted>
-              <List.Item
-                as="a"
-                href="https://www.github.com/joaquimbarreto"
-                target="_blank"
-              >
-                Github
-              </List.Item>
-              <List.Item as={Link} to="projects">
-                Projects
-              </List.Item>
-              <List.Item as={Link} to="cv">
-                CV
-              </List.Item>
-              <List.Item as={Link} to="contact">
-                Contact Me
-              </List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={5}>
-            <Header inverted as="h4" content="Blogs" />
-            <List link inverted>
-              <List.Item
-                as="a"
-                href="https://fullstacksoftware.engineer"
-                target="_blank"
-              >
-                fullstacksoftware.engineer
-              </List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={7}>
-            <Header as="h4" inverted>
-              Joaquim Barreto
-            </Header>
-            <p>Junior Full-stack Software Engineer</p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Container>
-  </Segment>
-);
+const Footer = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Segment inverted vertical style={{ padding: '5em 0em' }}>
+      <Container>
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Header inverted as="h4" content="About" />
+              <List link inverted>
+                <List.Item
+                  as="a"
+                  href="https://www.github.com/joaquimbarreto"
+                  target="_blank"
+                >
+                  Github
+                </List.Item>
+                <List.Item as={Link} to="projects">
+                  {t('projects')}
+                </List.Item>
+                <List.Item as={Link} to="cv">
+                  CV
+                </List.Item>
+                <List.Item as={Link} to="contact">
+                  {t('contact')}
+                </List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={5}>
+              <Header inverted as="h4" content="Blogs" />
+              <List link inverted>
+                <List.Item
+                  as="a"
+                  href="https://fullstacksoftware.engineer"
+                  target="_blank"
+                >
+                  fullstacksoftware.engineer
+                </List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header as="h4" inverted>
+                Joaquim Barreto
+              </Header>
+              <p>{t('job_title')}</p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Segment>
+  );
+};
 export default Footer;
